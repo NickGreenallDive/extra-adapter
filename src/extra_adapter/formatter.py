9f17ae8745c7c5296e759a31c524cd7bf96a37db
Ivo Simonsmeier <simonsmeier@dive-solutions.de>
2022-01-25 16:55:37 +0100

    Extract output key check and make dummy record a global constant.

--- src/extra_adapter/formatter.py
+++ src/extra_adapter/formatter.py
@@ -2,6 +2,11 @@ import json
 import logging
 
 META_KEYS = ["filename", "levelname", "name", "lineno"]
+_DUMMY_RECORD = logging.LogRecord(None, None, None, None, None, None, None)  # type: ignore
+
+
+def is_output_key(key: str):
+    return key not in _DUMMY_RECORD.__dict__ or key in META_KEYS
 
 
 class StringFormatter(logging.Formatter):
@@ -27,14 +32,9 @@ class StringFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord):
         record.msg = record.getMessage()
-        dummy = logging.LogRecord(None, None, None, None, None, None, None)  # type: ignore
         record.args = tuple()
         extra_messages = "  ".join(
-            [
-                f"{k}={v}"
-                for k, v in record.__dict__.items()
-                if k not in dummy.__dict__ or k in META_KEYS
-            ]
+            [f"{k}={v}" for k, v in record.__dict__.items() if is_output_key(k)]
         )
         return record.msg + ": " + extra_messages
 
@@ -63,10 +63,9 @@ class JSONFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord):
         record.msg = record.getMessage()
-        dummy = logging.LogRecord(None, None, None, None, None, None, None)  # type: ignore
         record.args = tuple()
         output = dict(message=record.msg)
         for key, value in record.__dict__.items():
-            if key not in dummy.__dict__ or key in META_KEYS:
+            if is_output_key(key):
                 output[key] = str(value)
         return json.dumps(output)
